a7dab4a84ba4eb178701f43aeaf037edd2a13784
AidaGHoul <aida-yousfi02@hotmail.com>
2025-09-25 11:12:23 +0200

    commit one

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+import tensorflow_hub as hub
+import os
+
+os.environ["TFHUB_CACHE_DIR"] = "./tfhub_cache"
+
+app = FastAPI()
+
+# Charger le modèle SVC et le MultiLabelBinarizer
+with open("model_SVC_USE/model.pkl", "rb") as f:
+    svc_model = pickle.load(f)
+with open("model_SVC_USE/mlb.pkl", "rb") as f:
+    mlb = pickle.load(f)
+
+# Charger Universal Sentence Encoder
+use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+class TextRequest(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict_tags(req: TextRequest):
+    embedding = use_model([req.text]).numpy()
+    y_pred = svc_model.predict(embedding)
+    print("y_pred:", y_pred)  # Pour voir la sortie
+    tags = mlb.inverse_transform(y_pred)[0]
+    print("tags:", tags)      # Pour voir les tags
+    return {"tags": tags}
